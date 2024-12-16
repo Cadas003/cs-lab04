59ba10f16be6b55863b1b5adab0080ba43a35e41
Alice (RyzhenkoAI) <alice@prosto.com>
2024-12-16 16:49:43 +0300

    code: добавлен вывод максимума

--- main.cpp
+++ main.cpp
@@ -5,11 +5,12 @@ using namespace std;
 int main()
 {
     cout << "Enter A and B: ";
-    int a, b;
+    double a, b;
     cin >> a >> b;
     cout << "A + B = " << a + b << '\n'
          << "A - B = " << a - b << '\n'
          << "A * B = " << a * b << '\n'
-         << "A / B = " << a / b << '\n';
+         << "A / B = " << a / b << '\n'
+         << "max(A, B) = " << max(a, b) << '\n';
     return 0;
 }
